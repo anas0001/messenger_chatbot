ade05c8e7f520508afee891a98c212627c79999c
Muhammad Anas Suleman <muhammad.anas1995@gmail.com>
2019-08-29 08:12:25 +0500

    trying

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def webhook():
 	#get_started = requests.post(fb_api,params=token_dict,{"get_started":{"payload":"some bitch clicked the get started button"}})
 	persistent_menu_json = {"persistent_menu":[{"locale":"default","composer_input_disabled":False,"call_to_actions":[{"type":"postback","title":"Talk to an agent","payload":"CARE_HELP"},{"type":"postback","title":"Outfit suggestions","payload":"CURATION"},{"type":"web_url","title":"Shop now","url":"https://www.originalcoastclothing.com/","webview_height_ratio":"full"}]}]}
 	persistent = requests.post(profile_api, params=token_dict, json= persistent_menu_json)
-	print(persistent)
+	print("persistent",persistent)
 	if data['object'] == "page":
 		entries = data['entry']
 
@@ -45,7 +45,7 @@ def webhook():
 						query = messaging_event['message']['text']
 						response = requests.post(fb_api,params=token_dict, json={"message": {"text": "hello"}, "recipient": {"id": sender_id}, "notification_type": "REGULAR", "messaging_type": "RESPONSE"})
 						response2 = requests.post(fb_api,params=token_dict, json={"recipient":{"id":sender_id}, "messaging_type": "RESPONSE","message":{"text": "Pick a color:","quick_replies":[{"content_type":"text","title":"Red","payload":"red"},{"content_type":"text","title":"Green", "payload":"green"}]}})
-
+						print("hello",response)
 						#print(response)
 						#result = response.json()
 						#print(result)
