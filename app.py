0da5ddda878cd0451349c7c93165290317eca8ec
Muhammad Anas Suleman <muhammad.anas1995@gmail.com>
2019-08-31 23:21:00 +0500

    miscellaneous

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def webhook():
 
 					elif messaging_event['postback'].get('payload') == 'courses.questions':
 						response = requests.post(fb_api,params=token_dict, json={"message": {"text": courses_questions}, "recipient": {"id": sender_id}, "notification_type": "REGULAR", "messaging_type": "RESPONSE"})
-						response2 = requests.post(fb_api,params=token_dict, json={"recipient":{"id": id}, "messaging_type": "RESPONSE","message":{"text": "You can ask me about DigiSkills Training program.","quick_replies":[{"content_type":"text","title":"Continue Reading 🤖","payload":"nigga clicked continue reading"}]}})
+						#response2 = requests.post(fb_api,params=token_dict, json={"recipient":{"id": id}, "messaging_type": "RESPONSE","message":{"text": "You can ask me about DigiSkills Training program.","quick_replies":[{"content_type":"text","title":"Continue Reading 🤖","payload":"nigga clicked continue reading"}]}})
 						return "ok", 200
 
 					elif messaging_event['postback'].get('payload') == 'courses.details':
