3be992a0a072fc902a0af2736836e5f61d561f9e
Muhammad Anas Suleman <muhammad.anas1995@gmail.com>
2020-02-17 20:12:41 +0500

    trying to better understand the response from psid

--- app.py
+++ app.py
@@ -104,6 +104,7 @@ def webhook():
 				recipient_id = messaging_event['recipient']['id']
 
 				# using a GET request to extract the user's information. In the below lines of code, we get the name of the user. Now, we can call the user by his name.
+				print("++++++++++++++++++++++++++++++sender ID:", sender_id)
 				response = requests.get(psid_url+sender_id + "?fields=name&access_token=" + PAGE_ACCESS_TOKEN)
 				print("------------------------------response------------------------------")
 				print(response)
