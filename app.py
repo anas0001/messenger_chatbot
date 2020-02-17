07711d4a1c56b7b3ee23e41ae7a25d5ae1dcd154
Muhammad Anas Suleman <muhammad.anas1995@gmail.com>
2020-02-17 18:02:37 +0500

    checking profile api response

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def verify():
 @app.route('/', methods=['POST'])
 def webhook():
 	# this print statement checks what input has been placed by the user. It is here for debugging purposes only.
-	print(request.data)
+	#print(request.data)
 
 	# this line of code extracts the json out of the user input.
 	data = request.get_json()
@@ -106,6 +106,7 @@ def webhook():
 				# using a GET request to extract the user's information. In the below lines of code, we get the name of the user. Now, we can call the user by his name.
 				response = requests.get(psid_url+sender_id + "?fields=name&access_token=" + PAGE_ACCESS_TOKEN)
 				user_json = json.loads(response.content)
+				print(user_json)
 				user_name = user_json["name"]
 
 				# this is the json for another gallery
