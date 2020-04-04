7f80fa6f0f5236aa27732f58941cb7c4cff0efa3
Muhammad Anas Suleman <muhammad.anas1995@gmail.com>
2020-04-05 04:47:36 +0500

    nothing significant

--- app.py
+++ app.py
@@ -71,10 +71,10 @@ def verify():
 @app.route('/', methods=['POST'])
 def webhook():
 	# this print statement checks what input has been placed by the user. It is here for debugging purposes only.
-	print("-----------------------------------------------------------------------------------------------")
+	print("start-----------------------------------------------------------------------------------------------")
 	print(request.data)
-	print("-----------------------------------------------------------------------------------------------")
-	
+	print("end-----------------------------------------------------------------------------------------------")
+
 	# this line of code extracts the json out of the user input.
 	data = request.get_json()
 
